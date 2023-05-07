1eb009df9fb08f9988389a0d045daa19ca3d6a1b
philip.figueroa@obf.ateneo.edu <philip.figueroa@obf.ateneo.edu>
2023-05-07 21:27:25 +0800

    almost done

--- payroll_app/views.py
+++ payroll_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-
+from django.contrib import messages
 # Create your views here.
 def employees(request):
     emp = Employee.objects.all()
@@ -15,7 +15,7 @@ def overtime(request, pk):
         return redirect('employees') 
     else:
         return redirect('employees') 
-       
+
 def create_employee(request):
     emp = Employee.objects.all()
     if request.method == "POST":
@@ -25,11 +25,30 @@ def create_employee(request):
         a = request.POST.get('allowance')
         if a == "":
             a = 0
+
+        if Employee.objects.filter(id_number=id).exists():
+            messages.error(request, "An employee with this ID number already exists")
+            return redirect('create_employee')
         
-        Employee.objects.create(name=n, id_number=id, rate=r, allowance=a)
-        return redirect('employees')
+        else:
+            Employee.objects.create(name=n, id_number=id, rate=r, allowance=a)
+            return redirect('employees')
     else:
-        return render(request, 'payroll_app/create_employee.html')
+        return render(request, 'payroll_app/create_employee.html')    
+# def create_employee(request):
+#     emp = Employee.objects.all()
+#     if request.method == "POST":
+#         n = request.POST.get('name')
+#         id = request.POST.get('idnum')
+#         r = request.POST.get('rate')
+#         a = request.POST.get('allowance')
+#         if a == "":
+#             a = 0
+        
+#         Employee.objects.create(name=n, id_number=id, rate=r, allowance=a)
+#         return redirect('employees')
+#     else:
+#         return render(request, 'payroll_app/create_employee.html')
 
 def delete_employee(request, pk):
     Employee.objects.filter(pk=pk).delete()
@@ -44,89 +63,119 @@ def update_employee(request, pk):
         r = request.POST.get('rate')
         a = request.POST.get('allowance')
 
-        Employee.objects.filter(pk=pk).update(name=n, id_number=id, rate=r, allowance=a )
-        return redirect('employees')
+        if Employee.objects.get(id_number=id).getID() == id:
+            Employee.objects.filter(pk=pk).update(name=n, id_number=id, rate=r, allowance=a)
+            return redirect('employees')
+        
+        elif Employee.objects.filter(id_number=id).exists():
+            messages.error(request, "An employee with this ID number already exists")
+            return redirect('update_employee', pk=pk)
+        
+        else:
+            Employee.objects.filter(pk=pk).update(name=n, id_number=id, rate=r, allowance=a)
+            return redirect('employees')
     else: 
         return render(request, 'payroll_app/update_employee.html', {'emp':emp})
     
-def view_payslips(request):
+def payslips(request):
     emp = Employee.objects.all()
-    return render(request, 'payroll_app/payslips.html', {'emp':emp})
+    payslip_test = Payslip.objects.all()
+    return render(request, 'payroll_app/payslips.html', {'emp':emp, 'ptest':payslip_test,})
 
 def payslip_submit(request):
     emp = Employee.objects.all()
     inp_for = request.POST.get('payslip_for')
     inp_month = request.POST.get('month')
-    c = 0
-    d = None
     inp_year = request.POST.get('year')
     month30 = ["4", "6", "9", "11"]
     leap_status = None
     inp_cycle = request.POST.get('cycle')
 
     # Leap year check
-    if int(inp_year) % 4 == 0:
-        if int(inp_year) % 100 == 0:
-            if int(inp_year) % 400 == 0:
+    if request.method == "POST":
+        if int(inp_year) % 4 == 0:
+            if int(inp_year) % 100 == 0:
+                if int(inp_year) % 400 == 0:
+                    leap_status = "LEAP!"
+                else:
+                    leap_status = "NOT LEAP!"
+            else:    
                 leap_status = "LEAP!"
-            else:
-                leap_status = "NOT LEAP!"
-        else:    
-            leap_status = "LEAP!"
-    else:
-        leap_status = "NOT LEAP!"
+        else:
+            leap_status = "NOT LEAP!"
+   
  
 
-    day_range = None
+        day_range = None
 
-    if inp_cycle == "1":
-        day_range = "1-15"
-    if inp_cycle == "2":
-        if inp_month in month30:
-            day_range = "16-30"
-        elif inp_month == "2":
-            if leap_status == "LEAP!":
-                day_range = "16-29"
+        if inp_cycle == "1":
+            day_range = "1-15"
+        if inp_cycle == "2":
+            if inp_month in month30:
+                day_range = "16-30"
+            elif inp_month == "2":
+                if leap_status == "LEAP!":
+                    day_range = "16-29"
+                else:
+                    day_range = "16-28"
             else:
-                day_range = "16-28"
-        else:
-            day_range = "16-31"
+                day_range = "16-31"
 
-    
-    if inp_for != "all":
-        active_id = Employee.objects.get(id_number=inp_for)
-        gross_pay = active_id.getRate() + active_id.getAllowance() + active_id.getOvertime()
-        philhealth = active_id.getRate() * 0.04
-        sss = active_id * 0.045
+        
+        if inp_for != "all":
+            active_id = Employee.objects.get(id_number=inp_for)
+            base_pay = active_id.getRate()/2
+            gross_pay = base_pay + active_id.getAllowance() + active_id.getOvertime()
+
+            if inp_cycle == "2":
+                philhealth = active_id.getRate() * 0.04
+                sss = active_id.getRate() * 0.045
+                tax = (gross_pay - sss - philhealth) * 0.2
+                total_deductions = tax + sss + philhealth
+                net_pay = gross_pay - total_deductions
+                pag_ibig = 0
+            else:
+                philhealth = 0
+                sss = 0
+                pag_ibig = 100
+                tax = (gross_pay - pag_ibig)*0.2
+                total_deductions = tax + pag_ibig
+                net_pay = gross_pay - total_deductions
 
 
-    context = {
-        'b':inp_month,
-        'd':d,
-        'l':leap_status,
-        'r':day_range,
-        'emp': emp,
-    }
+            Payslip.objects.create(id_number=active_id,
+                                month=inp_month,
+                                date_range=day_range,
+                                year=inp_year,
+                                pay_cycle=inp_cycle,
+                                rate=active_id.getRate(),
+                                earnings_allowance=active_id.getAllowance(),
+                                deductions_tax=tax,
+                                deductions_health=philhealth,
+                                pag_ibig=pag_ibig,
+                                sss=sss,
+                                overtime=active_id.getOvertime(),
+                                total_pay=net_pay,
+                                )
+
+        payslip_test = Payslip.objects.all()
+        context = {
+            'b':inp_month,
+            'l':leap_status,
+            'r':day_range,
+            'emp': emp,
+            'ptest':payslip_test,
+        }
+        
+        return render(request, 'payroll_app/test.html', context)
+    else:
+        pass
+
+def view_payslip(request, pk):
+    d = get_object_or_404(Payslip, pk=pk)
+    return render(request, 'payroll_app/view_payslip.html', {'d':d})
 
 
-    # elif inp_for == "all":  
-    #     for i in emp:
-    #         c+=1 #replace with create payslip object  
-    # else:
-    #     pass
-
-    # context = {
-    #     'a': inp_for,
-    #     'b': b,
-    #     'c': c,
-    #     'd': d,
-    #     'e':gross_pay,
-    # }
-
-    
-    
-
-    return render(request, 'payroll_app/test.html', context)
 
 # def test(request, pk):
 #      Employee.objects.get(pk=pk).resetOvertime()
